90c4c79fe3a3cfb37ee9b11a2210fad4b3a481c5
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2025-01-16 20:00:31 -0300

    estamos com problema de senha de banco. a minha senha é 'aluno' e a sua é 'enzo123', estava testando se era isso que estava dando erro

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
     host: '127.0.0.1', 
     port: 3306,  
     user: 'root', 
-    password: 'enzo321', 
+    password: 'enzo123', 
     database: 'clubfit',
   });
 
