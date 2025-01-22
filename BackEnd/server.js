c50103436c082bf4ea4cd1150ca0710f9f287dd8
Enzo Martins <enzoea256@gmail.com>
2025-01-22 15:51:01 -0300

    Teste

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
     host: '127.0.0.1', 
     port: 3306,  
     user: 'root', 
-    password: 'aluno', 
+    password: 'enzo123', 
     database: 'clubfit',
   });
 
