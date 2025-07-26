bdbb07c4e64237d8405f022d6f798e85c302ec67
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2025-07-26 00:30:46 -0300

    feat: Criando controller para cadastrar usuario

---
+++ BackEnd/src/controllers/UsuarioControllers.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { Usuario } from "../models/Usuario";
+
+export const LoginUserControllers = async (req: Request, res: Response) =>  {
+    const {nome, email, senha, objetivo, telefone, dataNascimento} = req.body;
+    console.log(`Cadastrando usuario: ${nome}, email: ${email}`);
+
+    if (!nome || !email){
+        res.status(400).json({error: 'Nome e email são obrigatório'});
+        return;
+    }
+
+    try {
+        const novoUser = await Usuario.RegisterUser({nome, email, senha, objetivo, telefone, dataNascimento});
+        res.status(201).json(novoUser);
+
+    } catch (error) {
+        console.error("Erro ao cadastrar usuário:", error);
+        const MensagemError = error instanceof Error ? error.message : 'Erro desconhecido';
+        console.log(`Error: ${error} ${MensagemError}`);
+        res.status(500).json({error: 'Erro ao cadastrar usuario'})
+    }
+}
