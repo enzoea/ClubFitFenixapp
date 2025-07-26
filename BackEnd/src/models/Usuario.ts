8e70ac93fed5896544cd2a676508882d80db3a8d
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2025-07-25 23:32:03 -0300

    feat: adicionando a classe usuario e tambem uma função assincrona salvar dados do usuario

--- BackEnd/src/models/Usuario.ts
+++ BackEnd/src/models/Usuario.ts
@@ -2,3 +2,31 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+interface IUsuario {
+    id?: number;
+    nome: string;
+    email: string;
+    senha: string;
+    objetivo: string;
+    telefone: string;
+    dataNascimento: Date;
+    
+    
+}
+
+export class Usuario {
+    static async RegisterUser(usuario: IUsuario): Promise<IUsuario> {
+        const novoUsuario = await prisma.usuario.create({
+            data: {
+                nome: usuario.nome,
+                email: usuario.email,
+                senha: usuario.senha,
+                objetivo: usuario.objetivo,
+                telefone: usuario.telefone,
+                dataNascimento: usuario.dataNascimento
+            }
+        });
+    console.log(`Usuario ${usuario.nome} cadastrado com sucesso!`);
+    return novoUsuario
+    }
+}
