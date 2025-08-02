d1b9a84447bf1822161cfe1280552fc3ba43cad5
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2025-08-01 21:36:27 -0300

    feat: rotas

--- BackEnd/src/routes/Profissional.routes.ts
+++ BackEnd/src/routes/Profissional.routes.ts
@@ -1,10 +1,12 @@
 import  Express  from "express";
-import { RegisterProfControllers, LoginProfController,getUserByIdControllers } from "../controllers/ProfissionalControllers";
+import { RegisterProfControllers, LoginProfController,getUserByIdControllers, deleteUserCOntrollers, UpdateUserControllers } from "../controllers/ProfissionalControllers";
+
 
 const Profrouter = Express.Router();
 
 Profrouter.post('/register', RegisterProfControllers);
 Profrouter.post('/login', LoginProfController);
 Profrouter.get('/:id', getUserByIdControllers);
-
+Profrouter.put('/update/:id', UpdateUserControllers);
+Profrouter.delete('/delete/:id', deleteUserCOntrollers);
 export default Profrouter
